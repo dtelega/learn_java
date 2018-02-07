25be2fb72c84fbd44406405d44c600c5a91cefe6
dtelega <dima.hannasay@gmail.com>
2018-02-07 19:03:35 +0200

    find smallest int

---
+++ Find the smallest integer in the array/SmallestIntegerFinder.java
@@ -0,0 +1,9 @@
+public class SmallestIntegerFinder {
+    public static int findSmallestInt(int[] args) {
+      int min = args[0];
+      for (int i = 1; i < args.length; i++)
+        if (args[i] < min)
+          min = args[i];
+      return min;
+    }
+}
