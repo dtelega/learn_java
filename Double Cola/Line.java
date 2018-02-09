c033937016a534a56f6d575a692d2021fb7ac50b
dtelega <dima.hannasay@gmail.com>
2018-02-09 19:21:27 +0200

    best practices added

--- Double Cola/Line.java
+++ Double Cola/Line.java
@@ -16,3 +16,17 @@ public class Line {
      }
 }
 
+/*
+
+// Best Practices
+
+public class Line {
+  public static String WhoIsNext(String[] names, int n){
+    while ( n > names.length){
+      n = (n - (names.length - 1)) / 2;
+    }
+    return names[n-1];
+  }
+}
+
+*/
